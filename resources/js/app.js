c1e7e72fd80f19137c7cf65658cb0c29edec1712
ruhi16 <hndas16@gmail.com>
2019-09-13 09:10:09 +0530

    vue implemented

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,31 @@
 require('./bootstrap');
 
-window.Vue = require('vue');
+import Vue from 'vue';
+// window.Vue = require('vue');
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+import VueRouter from 'vue-router';
 
+Vue.use(VueRouter);
+import Example from './components/ExampleComponent.vue';
+// const Example = require('./components/ExampleComponent.vue');
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+
+
+const routes = [
+    {
+        path:'/example',
+        component: Example
+    }
+];
+
+const router = new VueRouter({
+    routes
+});
 
 const app = new Vue({
     el: '#app',
+    router
+
+    // render: h => h(App)
 });
