3f370a8d34a1e903cbac972a2b69fea278f2bb14
ruhi16 <hndas16@gmail.com>
2019-09-24 08:30:23 +0530

    passport implemented

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -12,17 +12,12 @@
     </v-btn>
     
     <v-row justify="space-around">
-    <v-icon>mdi-anchor</v-icon>
-
-    <v-icon>mdi-xbox-controller</v-icon>
-
-    <v-icon>mdi-watch</v-icon>
-
-    <v-icon>mdi-tilde</v-icon>
-
-    <v-icon>mdi-tennis</v-icon>
-
-    <v-icon>mdi-mouse</v-icon>
+      <v-icon>mdi-anchor</v-icon>
+      <v-icon>mdi-xbox-controller</v-icon>
+      <v-icon>mdi-watch</v-icon>
+      <v-icon>mdi-tilde</v-icon>
+      <v-icon>mdi-tennis</v-icon>
+      <v-icon>mdi-mouse</v-icon>
     </v-row>
   <!-- <br>
     <v-spacer></v-spacer>
